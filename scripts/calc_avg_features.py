fix average features script

scripts/calc_avg_features.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 import sys
 import torchvision.transforms as t
-from torch.utils.data import DataLoader, dataloader
+from torch.utils.data import DataLoader
 
 sys.path.append(os.path.join(os.getcwd()))
 
@@ -12,7 +12,7 @@ from src.data import SimpleDataset
 from src.network import VGGEncoder
 
 
-def load_data(path, resize_size, crop_size):
+def load_data(path, resize_size, crop_size, batch_size):
     if resize_size == 0:
         transforms = t.Compose(
             [
@@ -32,7 +32,9 @@ def load_data(path, resize_size, crop_size):
         )
 
     dataset = SimpleDataset(path, [0, len(os.listdir(path))], transforms)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=False, pin_memory=True
+    )
 
     print(f"Loading data from {path}\nLoaded {len(dataset)} images")
 
@@ -48,7 +50,7 @@ if __name__ == "__main__":
         "-o", "--output", type=Path, help="Path to save to", required=True
     )
     parser.add_argument(
-        "-n", type=int, default=100, help="Number of samples to average from"
+        "--n_batches", type=int, default=8, help="Number of batches to average from"
     )
     parser.add_argument(
         "--resize_size",
@@ -64,44 +66,52 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    max_n = args.n_batches * args.batch_size
+
     # Load data
-    dataloader = load_data(args.input, args.resize_size, args.crop_size)
+    dataloader = load_data(
+        args.input, args.resize_size, args.crop_size, args.batch_size
+    )
 
     # Load model
     encoder = VGGEncoder()
     encoder = encoder.to(device)
 
-    features = {
-        "f1": [],
-        "f2": [],
-        "f3": [],
-        "f4": [],
-    }
+    features = {}
 
-    # Sample n times
-    print(f"Getting average from {args.n} samples...")
+    # Sample max_n times
+    print(f"Getting average from {max_n} samples...")
     with torch.no_grad():
         n = 0
-        while n < args.n:
+        while n < max_n:
             for img in dataloader:
-                n += args.batch_size
-                if n > args.n:
+                if n >= max_n:
                     break
 
                 img = img.to(device)
 
                 f1, f2, f3, f4 = encoder(img, return_all=True)
 
-                features["f1"].append(f1.detach().cpu())
-                features["f2"].append(f2.detach().cpu())
-                features["f3"].append(f3.detach().cpu())
-                features["f4"].append(f4.detach().cpu())
+                if features == {}:
+                    features["f1"] = f1.sum(0).detach().cpu()
+                    features["f2"] = f2.sum(0).detach().cpu()
+                    features["f3"] = f3.sum(0).detach().cpu()
+                    features["f4"] = f4.sum(0).detach().cpu()
+                else:
+                    features["f1"] += f1.sum(0).detach().cpu()
+                    features["f2"] += f2.sum(0).detach().cpu()
+                    features["f3"] += f3.sum(0).detach().cpu()
+                    features["f4"] += f4.sum(0).detach().cpu()
+
+                n += args.batch_size
 
     # Average samples
-    features["f1"] = torch.mean(torch.cat(features["f1"][: args.n], dim=0), dim=0)
-    features["f2"] = torch.mean(torch.cat(features["f2"][: args.n], dim=0), dim=0)
-    features["f3"] = torch.mean(torch.cat(features["f3"][: args.n], dim=0), dim=0)
-    features["f4"] = torch.mean(torch.cat(features["f4"][: args.n], dim=0), dim=0)
+    features["f1"] /= max_n
+    features["f2"] /= max_n
+    features["f3"] /= max_n
+    features["f4"] /= max_n
 
-    torch.save(features, args.output)
+    torch.save(
+        [features["f1"], features["f2"], features["f3"], features["f4"]], args.output
+    )
     print(f"Saved to {args.output}")